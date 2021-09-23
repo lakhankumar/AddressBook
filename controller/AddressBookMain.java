4824fe63b126706466c99d80e29af0423c12b734
lakhankumar <lakhankumar997@gmail.com>
2021-09-23 19:26:28 +0530

    create contact in address book

--- controller/AddressBookMain.java
+++ controller/AddressBookMain.java
@@ -1,7 +1,14 @@
 package controller;
 
+import model.AddressBookModel;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to address book");
+        AddressBookModel addressBookModel = new AddressBookModel("lakhan","torne","attapur",
+                                            "hyderabd","telangana","500048","905904xxxx",
+                                            "lakhan@gmail.com");
+        System.out.println(addressBookModel);
     }
+
 }
--- model/AddressBookModel.java
+++ model/AddressBookModel.java
@@ -1,5 +1,39 @@
 package model;
 
 public class AddressBookModel {
+    String firstName;
+    String lastNames;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
+
+    public AddressBookModel(String firstName, String lastNames, String address, String city,
+                            String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastNames = lastNames;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+    
+    @Override
+    public String toString() {
+        return "AddressBookModel{" +
+                "firstName='" + firstName + '\'' +
+                ", lastNames='" + lastNames + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 
 }
